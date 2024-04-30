a50fbcd09e0e62be224ed3d3f5339682072573d5
JiajiaLi04 <lijiajia04@outlook.com>
2024-04-30 10:09:59 -0400

    fix bugs

--- plot_gaze_density_scenarios.py
+++ plot_gaze_density_scenarios.py
@@ -1,64 +1,57 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.rcParams["font.family"] = "Times New Roman"
 
-df_bike2crosswalk = pd.read_excel(
-    'Whole_excels/bike2crosswalk.xlsx', engine='openpyxl',
-    usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
-
-df_downhill = pd.read_excel('Whole_excels/downhill.xlsx', engine='openpyxl',
-                            usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
-
-df_intersection = pd.read_excel('Whole_excels/intersection.xlsx', engine='openpyxl',
+df_bike2crosswalk = pd.read_csv('Outputs/bike2crosswalk.csv',
                                 usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
+df_downhill = pd.read_csv('Outputs/downhill.csv',
+                          usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
 
-df_road_fixtures = pd.read_excel('Whole_excels/road_fixtures.xlsx', engine='openpyxl',
-                                 usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
+df_intersection = pd.read_csv('Outputs/intersection.csv',
+                              usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
 
-df_road2bikelane = pd.read_excel('Whole_excels/road2bikelane.xlsx', engine='openpyxl',
-                                 usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
+# df_road_fixtures = pd.read_csv('Outputs/road_fixtures.csv',
+#                                usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
 
-df_close_proximity = pd.read_excel('Whole_excels/close_proximity.xlsx', engine='openpyxl',
-    usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
-
-df_occupied_crosswalk = pd.read_excel('Whole_excels/occupied_crosswalk.xlsx', engine='openpyxl',
-    usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
-
-df_passing_bus = pd.read_excel('Whole_excels/passing_bus.xlsx', engine='openpyxl',
+df_road2bikelane = pd.read_csv('Outputs/road2bikelane.csv',
                                usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
 
-df_passing_pedestrian = pd.read_excel('Whole_excels/passing_pedestrian.xlsx', engine='openpyxl',
-    usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
+df_close_proximity = pd.read_csv('Outputs/close_proximity.csv',
+                                 usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
+
+df_occupied_crosswalk = pd.read_csv('Outputs/occupied_crosswalk.csv',
+                                    usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
+
+df_passing_bus = pd.read_csv('Outputs/passing_bus.csv',
+                             usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
+df_passing_pedestrian = pd.read_csv('Outputs/passing_pedestrian.csv',
+                                    usecols=['Gaze point X [MCS px]', 'Gaze point Y [MCS px]'])
 
 dfs = [df_bike2crosswalk.dropna(), df_intersection.dropna(), df_occupied_crosswalk.dropna(),
-       df_downhill.dropna(), df_close_proximity.dropna(),
-       df_road_fixtures.dropna(), df_road2bikelane.dropna(),
+       df_downhill.dropna(), df_close_proximity.dropna(), df_road2bikelane.dropna(),
        df_passing_bus.dropna(), df_passing_pedestrian.dropna()]
 
 names = ['Bike lane to crosswalk', 'Intersection', 'Occupied crosswalk',
-         'Downhill', 'Close proximity',
-         'Road fixtures', 'Road to bike lane',
-         'Passing bus', 'Passing pedestrian']
+         'Downhill', 'Close proximity', 'Road to bike lane', 'Passing bus', 'Passing pedestrian']
 
 title_fontsize = 20
 common_title_fontsize = 20
 label_fontsize = 16
 cbar_fontsize = 16
 
-# Create a 3x3 grid of subplots
-fig, axes = plt.subplots(3, 3, figsize=(15, 15))
+# Create a 2x4 grid of subplots
+fig, axes = plt.subplots(2, 4, figsize=(15, 7.5))
 
 # Plotting each DataFrame's data in the grid
 for ax, df, name in zip(axes.flatten(), dfs, names):
     h = ax.hist2d(df['Gaze point X [MCS px]'], df['Gaze point Y [MCS px]'],
-                  bins=35, cmap='jet', range=[[800, 1200], [250, 750]])  # , range=[[600, 1200], [400, 700]]
+                  bins=35, cmap='jet', range=[[800, 1200], [250, 750]])
     ax.set_title(name, fontsize=title_fontsize)
     fig.colorbar(h[3], ax=ax).ax.tick_params(labelsize=cbar_fontsize)
 
-fig.text(0.5, 0.01, 'Horizontal gaze position (px)', ha='center', va='center', fontsize=common_title_fontsize)
+fig.text(0.5, 0.02, 'Horizontal gaze position (px)', ha='center', va='center', fontsize=common_title_fontsize)
 fig.text(0.01, 0.5, 'Vertical gaze position (px)', ha='center', va='center', rotation='vertical',
          fontsize=common_title_fontsize)
 
--- plot_gaze_entropy_facilities.py
+++ plot_gaze_entropy_facilities.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.rcParams["font.family"] = "Times New Roman"
 
@@ -20,71 +21,82 @@ data_walkways['event'] = 'Walkways'
 
 # Combine the data from all events into one DataFrame
 data_combined = pd.concat([data_sidewalk, data_walkways, data_roadwbikelane, data_roadwobikelane])
+data_combined_cleaned = data_combined[data_combined['gaze_transition_entropy'] <= 150]
+data_combined_cleaned = data_combined_cleaned[data_combined_cleaned['PRC'] >= 0.1]
+data_combined_cleaned = data_combined_cleaned[data_combined_cleaned['gaze_variablity_y'] <= 180]
+data_combined_cleaned = data_combined_cleaned[data_combined_cleaned['mean_fixation_length'] <= 0.5]
+data_combined_cleaned = data_combined_cleaned[data_combined_cleaned['number_fixation_per_second'] <= 7]
 
 
 def save_and_show_figure(fig, filename):
     fig.tight_layout()  # Adjust the layout
     fig.savefig(f'{filename}.png', dpi=600)
     fig.savefig(f'{filename}.pdf', dpi=600)
-    plt.show()
+    # plt.show()
 
-font_size = 16
-tick_size = 10
+font_size = 20
+tick_size = 16
+tick_size_x = 30
 degree = 0
+scatter_size = 4.5
 
-labels = ['Sidewalks', 'Walkways', 'Bike lane', 'No bike lane']
+labels = ['Sidewalks', 'Pedestrian trails', 'Bike lane', 'Road w.o. bike lane']
 
 # Stationary Gaze Entropy and Gaze Transition Entropy
 fig1, axs1 = plt.subplots(2, 1, figsize=(8, 6))  # 2 rows, 1 column
-sns.boxplot(x='event', y='stationary_gaze_entropy', data=data_combined, palette="Set3", ax=axs1[0])
-sns.swarmplot(x='event', y='stationary_gaze_entropy', data=data_combined, color=".25", dodge=True, ax=axs1[0])
+sns.boxplot(x='event', y='stationary_gaze_entropy', data=data_combined_cleaned, palette="Set3", ax=axs1[0], showfliers=False)
+sns.swarmplot(x='event', y='stationary_gaze_entropy', data=data_combined_cleaned, color=".25", size=scatter_size, dodge=True, ax=axs1[0])
 axs1[0].set_ylabel('Stationary gaze entropy', fontsize=font_size)
 axs1[0].set_xlabel('')
-axs1[0].tick_params(labelbottom=False)
-sns.boxplot(x='event', y='gaze_transition_entropy', data=data_combined, palette="Set3", ax=axs1[1])
-sns.swarmplot(x='event', y='gaze_transition_entropy', data=data_combined, color=".25", dodge=True, ax=axs1[1])
+axs1[0].tick_params(labelbottom=False, labelsize=tick_size)
+sns.boxplot(x='event', y='gaze_transition_entropy', data=data_combined_cleaned, palette="Set3", ax=axs1[1], showfliers=False)
+sns.swarmplot(x='event', y='gaze_transition_entropy', data=data_combined_cleaned, color=".25", size=scatter_size, dodge=True, ax=axs1[1])
 axs1[1].set_ylabel('Gaze transition entropy', fontsize=font_size)
-axs1[1].set_xticklabels(labels, rotation=degree, ha="center", fontsize=font_size)
+axs1[1].set_xticklabels(labels, rotation=degree, ha="center", fontsize=tick_size_x)
 axs1[1].set_xticks(range(len(labels)))
 axs1[1].set_xlabel('')
+axs1[1].tick_params(labelsize=tick_size)
 save_and_show_figure(fig1, 'Gaze_entropy_facilities/gaze_entropy_metrics')
 
 # Gaze Variability X and Gaze Variability Y
 fig2, axs2 = plt.subplots(2, 1, figsize=(8, 6))  # 2 rows, 1 column
-sns.boxplot(x='event', y='gaze_variablity_x', data=data_combined, palette="Set3", ax=axs2[0])
-sns.swarmplot(x='event', y='gaze_variablity_x', data=data_combined, color=".25", dodge=True, ax=axs2[0])
+sns.boxplot(x='event', y='gaze_variablity_x', data=data_combined_cleaned, palette="Set3", ax=axs2[0], showfliers=False)
+sns.swarmplot(x='event', y='gaze_variablity_x', data=data_combined_cleaned, color=".25", size=scatter_size, dodge=True, ax=axs2[0])
 axs2[0].set_ylabel('Gaze variability X', fontsize=font_size)
 axs2[0].set_xlabel('')
-axs2[0].tick_params(labelbottom=False)
-sns.boxplot(x='event', y='gaze_variablity_y', data=data_combined, palette="Set3", ax=axs2[1])
-sns.swarmplot(x='event', y='gaze_variablity_y', data=data_combined, color=".25", dodge=True, ax=axs2[1])
+axs2[0].tick_params(labelbottom=False, labelsize=tick_size)
+sns.boxplot(x='event', y='gaze_variablity_y', data=data_combined_cleaned, palette="Set3", ax=axs2[1], showfliers=False)
+sns.swarmplot(x='event', y='gaze_variablity_y', data=data_combined_cleaned, color=".25", size=scatter_size, dodge=True, ax=axs2[1])
 axs2[1].set_ylabel('Gaze variability Y', fontsize=font_size)
-axs2[1].set_xticklabels(labels, rotation=degree, ha="center", fontsize=font_size)
+axs2[1].set_xticklabels(labels, rotation=degree, ha="center", fontsize=tick_size_x)
 axs2[1].set_xticks(range(len(labels)))
 axs2[1].set_xlabel('')
+axs2[1].tick_params(labelsize=tick_size)
 save_and_show_figure(fig2, 'Gaze_entropy_facilities/gaze_variability_metrics')
 
 # Third Figure: PRC
 fig3, ax3 = plt.subplots(figsize=(8, 3))
-sns.boxplot(x='event', y='PRC', data=data_combined, palette="Set3", ax=ax3)
-sns.swarmplot(x='event', y='PRC', data=data_combined, color=".25", dodge=True, ax=ax3)
+sns.boxplot(x='event', y='PRC', data=data_combined_cleaned, palette="Set3", ax=ax3, showfliers=False)
+sns.swarmplot(x='event', y='PRC', data=data_combined_cleaned, color=".25", size=scatter_size, dodge=True, ax=ax3)
 ax3.set_ylabel('Percentage of road center', fontsize=font_size)
-ax3.set_xticklabels(labels, rotation=degree, ha="center", fontsize=font_size)
+ax3.set_xticklabels(labels, rotation=degree, ha="center", fontsize=tick_size_x)
 ax3.set_xticks(range(len(labels)))
 ax3.set_xlabel('')
+ax3.tick_params(labelsize=tick_size)
 save_and_show_figure(fig3, 'Gaze_entropy_facilities/prc_metric')
 
 # Fixation length and frequency
 fig4, axs4 = plt.subplots(2, 1, figsize=(8, 6))
-sns.boxplot(x='event', y='mean_fixation_length', data=data_combined, palette="Set3", ax=axs4[0])
-sns.swarmplot(x='event', y='mean_fixation_length', data=data_combined, color=".25", dodge=True, ax=axs4[0])
+sns.boxplot(x='event', y='mean_fixation_length', data=data_combined_cleaned, palette="Set3", ax=axs4[0], showfliers=False)
+sns.swarmplot(x='event', y='mean_fixation_length', data=data_combined_cleaned, color=".25", size=scatter_size, dodge=True, ax=axs4[0])
 axs4[0].set_ylabel('Mean fixation length (s)', fontsize=font_size)
 axs4[0].set_xlabel('')
-axs4[0].tick_params(labelbottom=False)
-sns.boxplot(x='event', y='number_fixation_per_second', data=data_combined, palette="Set3", ax=axs4[1])
-sns.swarmplot(x='event', y='number_fixation_per_second', data=data_combined, color=".25", dodge=True, ax=axs4[1])
+axs4[0].tick_params(labelbottom=False, labelsize=tick_size)
+sns.boxplot(x='event', y='number_fixation_per_second', data=data_combined_cleaned, palette="Set3", ax=axs4[1], showfliers=False)
+sns.swarmplot(x='event', y='number_fixation_per_second', data=data_combined_cleaned, color=".25", size=scatter_size, dodge=True, ax=axs4[1])
 axs4[1].set_ylabel('Fixations per second', fontsize=font_size)
-axs4[1].set_xticklabels(labels, rotation=degree, ha="center", fontsize=font_size)
+axs4[1].set_xticklabels(labels, rotation=degree, ha="center", fontsize=tick_size_x)
 axs4[1].set_xticks(range(len(labels)))
 axs4[1].set_xlabel('')
+axs4[1].tick_params(labelsize=tick_size)
 save_and_show_figure(fig4, 'Gaze_entropy_facilities/fixation_metrics')
